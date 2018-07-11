d35e59ad67088ecd66a80a53eb874e5255ac0181
junkrango <1614056450@qq.com>
2018-07-11 14:43:55 +0800

    done

---
+++ videos/sql.py
@@ -0,0 +1,18 @@
+# from videos.models import Category,Video,Push_date
+#
+# c = Category.objects.get(id=1)
+#
+# Video.objects.create(name='鬼妈妈 Coraline (2009)',
+#                      desc='This is desc',
+#                      author='null',
+#                      actor='null',
+#                      video_url='https://youku.cdn-163.com/20180428/6695_0cd1daf9/index.m3u8',
+#                      image_url='http://pianyuan.net/Uploads/Picture/litpic/07/21Jul2015233016.jpg',
+#                      category=c)
+def t():
+    if 1:
+        a=1
+        print(a)
+    a=2
+    print(a)
+t()
